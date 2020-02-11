380a79c0dd771dbe195ea0f6d58f70854a8a7194
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2020-02-11 19:38:56 +0000

    Add files via upload

--- cpp/load_kernel.hpp
+++ cpp/load_kernel.hpp
@@ -19,23 +19,27 @@ is the primary record of information. mvindex records the
 parent ion masses that have at least one entry in kindex.
 */
 
+// object to store kernel information from a binary JSON file object
 class jsObject
 {
 public:
-	jsObject(void)	{pm = 0; u = 0; h = 0; 
-			pBuffer = new unsigned char[1024*16-1];
-			pKey = new char[256];
+	jsObject(void)	{
+		pm = 0;
+		u = 0;
+		h = 0; 
+		pBuffer = new unsigned char[1024*16-1];
+		pKey = new char[256];
 	}
 	virtual ~jsObject(void)	{delete pKey;delete pBuffer;}
-	int32_t pm;
-	int32_t u;
-	int32_t h;
-	unsigned int size;
-	vector<int32_t> bs;
-	vector<int32_t> ys;
-	char *pKey;
-	unsigned char *pBuffer;
-	string key;
+	int32_t pm; // parent mass
+	int32_t u; // uid for kernel
+	int32_t h; // uid for first instance of a specifically modified peptide sequence
+	unsigned int size; // temporary value of an element's size
+	vector<int32_t> bs; // b fragment information
+	vector<int32_t> ys; // y fragment information
+	char *pKey; // buffer to store the key value of a binary JSON element
+	unsigned char *pBuffer; // buffer to read a binary JSON object
+	string key; // key value for a binary JSON element
 	void reset(void)	{
 		pm = 0;
 		u = 0;
@@ -48,12 +52,17 @@ public:
 class channel
 {
 public:
-	channel(void) {lower = 0; delta = 0; dead = true; mv = 0;}
+	channel(void) {
+		lower = 0;
+		delta = 0;
+		dead = true;
+		mv = 0;
+	}
 	virtual ~channel(void) {}
-	int32_t lower;
-	int32_t delta;
-	bool dead;
-	int32_t mv;
+	int32_t lower; // lower parent mass limit for a channel to be operative
+	int32_t delta; // mass offset of a channel
+	bool dead; // if dead = true, channel not available for use
+	int32_t mv; // reduced parent mass value of a channel
 };
 
 class kernels
@@ -70,7 +79,8 @@ public:
 	vector<phmap::flat_hash_map<kPair,vector<int32_t> > > kindex_a; //records the kernels containing the specified pair 
 	vector<phmap::flat_hash_set<int32_t> > mvindex_a; //set of parent masses with at least one entry in kindex 
 	size_t clength; // number of mass channels (a constant)
-	int32_t size(void)	{ // retrieve the number of values in kindex_a
+	// retrieve the number of values in kindex_a
+	int32_t size(void)	{
 		size_t sz = 0;
 		for(size_t a = 0; a < clength; a++)	{
 			sz += kindex_a[a].size();
@@ -120,7 +130,8 @@ public:
 	phmap::flat_hash_set<sPair> spairs; //map of spectrum (parent:fragment) mass pairs 
 	vector<channel> channels; //parent ion mass derived set of masses, e.g. [A0,A1,A2]
 	size_t clength; //number of elements in channels
-	void clean_up(void)	{ // free up memory and reset containers to zero length
+	// free up memory and reset containers to zero length
+	void clean_up(void)	{ 
 		sp_set.clear();
 		kerns.kindex_a.clear();
 		kerns.kindex_a.shrink_to_fit();
@@ -128,13 +139,15 @@ public:
 		kerns.mvindex_a.shrink_to_fit();
 		spairs.clear();
 	}
-	inline void add_hu(const int32_t _h,const int32_t _u)	{ // add a homologous kernel id/unique kernel id pair
+	// add a homologous kernel id/unique kernel id pair
+	inline void add_hu(const int32_t _h,const int32_t _u)	{
 		if(hu_set.find(_h) == hu_set.end())	{
 			hu_set[_h] = set<int32_t>();
 		}
 		hu_set[_h].insert(_u);
 	}
-	inline void check_and_update(const kPair& _pr,const int32_t _u)	{ //check a new pair/kernel unique id combination
+	//check a new pair/kernel unique id combination and record the information in the kernels object
+	inline void check_and_update(const kPair& _pr,const int32_t _u)	{
 		kPair pr = _pr;
 		for(size_t b = 0; b < clength;b++)	{
 			if(channels[b].dead)	{
