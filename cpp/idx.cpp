d411c67b32f019cce9de379d53eb36de7d7a40c9
Ron Beavis <47994639+RonBeavis@users.noreply.github.com>
2021-01-12 19:42:33 +0000

    Add files via upload

--- cpp/idx.cpp
+++ cpp/idx.cpp
@@ -110,27 +110,27 @@ inline bool exists (const std::string& name) {
 }
 
 void print_help(void)	{
-	cout << "\nusage:\t>idX -sFILE -kFILE -oFILE -f(low) -m(-1) -p(20)" << "\n";
+	cout << "\nusage:\t>idX -s FILE -k FILE -o FILE -f (low) -m (-1) -p (20)" << "\n";
 	cout << "\n1. Required parameters.\n";
-	cout << "-sFILE           spectrum peak list file path\n";
-	cout << "                 FILE:  MGF or CMN format peak list\n";
-	cout << "-kFILE           kernel file path\n";
-	cout << "                 FILE:  JSON lines format kernel list\n";
-	cout << "-oFILE           output file path\n";
-	cout << "                 FILE:  TSV format result file\n";
+	cout << "-s FILE           spectrum peak list file path\n";
+	cout << "                  FILE:  MGF or CMN format peak list\n";
+	cout << "-k FILE           kernel file path\n";
+	cout << "                  FILE:  JSON lines format kernel list\n";
+	cout << "-o FILE           output file path\n";
+	cout << "                  FILE:  TSV format result file\n";
 	cout << "\n2. Optional parameters.\n";
-	cout << "-fVALUE          fragment mass tolerance\n";
-	cout << "                 VALUE: low, medium or high\n";
-	cout << "                 default: low\n";
-	cout << "-mVALUE          maximum number of spectra to use\n";
-	cout << "                 VALUE:  positive integer or -1 if use all\n";
-	cout << "                 default: -1\n";
-	cout << "-pVALUE          parent mass tolerance (ppm)\n";
-	cout << "                 VALUE: positive integer\n";
-	cout << "                 default: 20\n";
+	cout << "-f VALUE          fragment mass tolerance\n";
+	cout << "                  VALUE: low, medium or high\n";
+	cout << "                  default: low\n";
+	cout << "-m VALUE          maximum number of spectra to use\n";
+	cout << "                  VALUE:  positive integer or -1 if use all\n";
+	cout << "                  default: -1\n";
+	cout << "-p VALUE          parent mass tolerance (ppm)\n";
+	cout << "                  VALUE: positive integer\n";
+	cout << "                  default: 20\n";
 	cout << "\n3. Other parameters.\n";
-	cout << "--version, -v    displays current version of idX\n";
-	cout << "--help, -h       displays this page\n";
+	cout << "-v, --version     displays current version of idX\n";
+	cout << "-h, --help        displays this page\n";
 	return;
 }
 
@@ -157,13 +157,20 @@ int load_params(map<string,string>& params,int argc,char* argv[])	{
 			exit(0);
 		} 
 	}
+	if(argc == 0)	{
+		print_help();
+		exit(0);
+	}
 	char *pvalue = NULL;
 	ostringstream strStream; //using ostringstream to avoid potentially unsafe sprintf
 	for(int i = 1; i < argc; i++)	{
-		flag[0] = argv[i][0];
-		flag[1] = argv[i][1];
-		flag[2] = '\0';
-		pvalue = argv[i]+2;
+		if(argv[i][0] == '-')	{
+			flag[0] = argv[i][0];
+			flag[1] = argv[i][1];
+			flag[2] = '\0';
+			continue;
+		}
+		pvalue = argv[i];
 		if(strcmp(flag,"-f") == 0)	{
 			int32_t fragment_tolerance = 300; // default fragment mass tolerance
 			try	{
